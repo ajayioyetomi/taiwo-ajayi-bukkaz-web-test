7cf8f6743f4d42c835d39c2d519ea93b150901ef
Taiwo Ajayi <ajayioyetomi@gmail.com>
2026-01-29 22:30:23 +0100

    fix login list alignment and added animation

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,7 @@
 import Logo from "../assets/logo.svg?react";
 const Login = () => {
   return (
-    <main className="p-4 sm:p-2 min-h-screen h-full sm:h-screen w-screen bg-black flex flex-col justify-center items-center">
+    <main className="zoom-in-out p-4 sm:p-2 min-h-screen h-full sm:h-screen w-screen bg-black flex flex-col justify-center items-center">
 
       <header className="flex flex-col items-center justify-center gap-4">
         <Logo />
@@ -9,14 +9,14 @@ const Login = () => {
       </header>
       <section className="flex flex-col-reverse md:flex-row sm:min-w-163 gap-4 mt-4 justify-flex-start sm:justify-between sm:items-center">
         <div>
-          <ol type="1" className="m-0 login-list" >
+          <ol type="1" className="m-0 login-list text-white" >
             <li>Open the logosum App on your phone</li>
             <li>Go to Profile → Settings</li>
             <li>Tap “Link TV Device</li>
             <li>
-              <ul className="m-0">
-                <li className="text-center"> Scan the QR Code displayed on your TV <br/> OR </li>
-                <li className="text-center">
+              <ul className="m-0 login-inner-list pl-2">
+                <li className="relative text-center  pl-4"> Scan the QR Code displayed on your TV <br/> OR </li>
+                <li className="relative text-center pl-4">
                   Enter the 14-digit login code shown below <br/>
                   <p className="py-2 text-[1.55rem] text-blue-100">QER-87T-OOI-87Q-PO</p>
                 </li>
